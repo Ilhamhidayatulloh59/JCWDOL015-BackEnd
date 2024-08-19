e12476197b009234bc0da4054e94314aecf26bd6
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-08-19 22:19:33 +0700

    continue expense

--- expense-api/src/controllers/expenseV2Controller.ts
+++ expense-api/src/controllers/expenseV2Controller.ts
@@ -32,3 +32,62 @@ export const getExpenseIdV2 = (req: Request, res: Response) => {
         })
     })
 }
+
+export const createExpenseV2 = (req: Request, res: Response) => {
+    const { title, type, category, nominal, date } = req.body
+    db.query(`INSERT INTO expense(title, type, category, nominal, date)
+    VALUES
+        ('${title}', '${type}', '${category}', ${nominal}, '${date}')
+    `, (err: QueryError) => {
+        if (err) {
+            return res.status(400).send({
+                status: 'error',
+                msg: err
+            })
+        }
+        return res.status(201).send({
+            status: 'ok',
+            msg: 'Expense created!'
+        })
+    })
+}
+
+export const updateExpenseV2 = (req: Request, res: Response) => {
+    // UPDATE expense SET title = "Beli Batagor", nominal = 15000, date = "2024-08-2" WHERE id = 2;
+    const query: string[] = []
+    for (let key in req.body) {
+        query.push(`${key} = "${req.body[key]}"`)
+    }
+    
+    db.query(`
+        UPDATE expense SET ${query.join(', ')} WHERE id = ${req.params.id}
+    `, (err: QueryError) => {
+        if (err) {
+            return res.status(400).send({
+                status: 'error',
+                msg: err
+            })
+        }
+        res.status(200).send({
+            status: 'ok',
+            msg: 'expense updated'
+        })
+    })
+}
+
+export const deleteExpenseV2 = (req: Request, res: Response) => {
+    db.query(`
+        DELETE FROM expense WHERE id = ${req.params.id}
+    `, (err: QueryError) => {
+        if (err) {
+            return res.status(400).send({
+                status: 'error',
+                msg: err
+            })
+        }
+        res.status(200).send({
+            status: 'ok',
+            msg: 'expense deleted'
+        })
+    })
+}
--- expense-api/src/routers/expenseV2Router.ts
+++ expense-api/src/routers/expenseV2Router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getExpenseIdV2, getExpenseV2 } from "../controllers/expenseV2Controller";
+import { createExpenseV2, deleteExpenseV2, getExpenseIdV2, getExpenseV2, updateExpenseV2 } from "../controllers/expenseV2Controller";
 
 const expenseRouterV2 = Router()
 
 expenseRouterV2.get('/', getExpenseV2)
 expenseRouterV2.get('/:id', getExpenseIdV2)
+expenseRouterV2.post('/', createExpenseV2)
+expenseRouterV2.patch('/:id', updateExpenseV2)
+expenseRouterV2.delete('/:id', deleteExpenseV2)
 
 export { expenseRouterV2 }
