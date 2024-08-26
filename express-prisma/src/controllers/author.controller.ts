08deb09f950d4bc8e3140a69ad3fef85e4c88e5b
Ilham Hidayatulloh <114216025+Ilhamhidayatulloh59@users.noreply.github.com>
2024-08-26 22:10:21 +0700

    auth

--- express-prisma/src/controllers/author.controller.ts
+++ express-prisma/src/controllers/author.controller.ts
@@ -1,17 +1,60 @@
 import { Request, Response } from "express";
 import prisma from "../prisma";
+import { compare, genSalt, hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
 
 export class AuthorController {
     async createAuthor(req: Request, res: Response) {
         try {
             const { name, email, password } = req.body
+
+            const existingAuthor = await prisma.author.findUnique({
+                where: { email: email }
+            })
+
+            if (existingAuthor) throw "email has been used !"
+
+            const salt = await genSalt(10)
+            const hashPassword = await hash(password, salt)
+
             const author = await prisma.author.create({
-                data: { name, email, password }
+                data: { name, email, password: hashPassword }
             })
             res.status(201).send({
                 status: 'ok',
                 msg: 'author created!',
                 author
+            })
+        } catch (err) {
+            res.status(400).send({
+                status: 'error',
+                msg: err
+            })
+        }
+    }
+
+    async loginAuthor(req: Request, res: Response) {
+        try {
+            const { email, password } = req.body
+
+            const existingAuthor = await prisma.author.findUnique({
+                where: { email: email }
+            })
+
+            if (!existingAuthor) throw "author not found !"
+
+            const isValidPass = await compare(password, existingAuthor.password)
+            
+            if (!isValidPass) throw "incorrect password !"
+
+            const payload = { id: existingAuthor.id, role: existingAuthor.role }
+            const token = sign(payload, process.env.SECRET_JWT!, { expiresIn: '1d' })
+
+            res.status(200).send({
+                status: 'ok',
+                msg: "login success !",
+                token,
+                author: existingAuthor
             })
         } catch (err) {
             res.status(400).send({
